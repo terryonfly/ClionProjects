7a8917888ade89b3ccbbed0772d8eabc88a06e33
terry <terry@rokid.com>
2015-12-11 15:04:42 +0800

    fps

--- IotRobotCtrl/main.c
+++ IotRobotCtrl/main.c
@@ -5,32 +5,87 @@
 #include <GL/glut.h>
 #endif
 
-void init(void){
-    glClearColor(0.0,0.0,0.0,0.0);
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> /* netdb is necessary for struct hostent */
+#include <math.h>
+#include <time.h>
+#include <i386/limits.h>
+
+static int day = 340; // day的变化：从0到359
+
+double CalFrequency()
+{
+    static int count;
+    static double save;
+    static clock_t last, current;
+    double timegap;
+
+    ++count;
+    if( count <= 50 )
+        return save;
+    count = 0;
+    last = current;
+    current = clock();
+    timegap = (current-last)/(double)CLK_TCK;
+    save = 50.0/timegap;
+    return save;
+}
+
+void myDisplay(void) {
+    double FPS = CalFrequency();
+    printf("FPS = %f\n", FPS);
+    glEnable(GL_DEPTH_TEST);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
     glMatrixMode(GL_PROJECTION);
-    glOrtho(-5,5,-5,5,5,15);
+    glLoadIdentity();
+    gluPerspective(75, 1, 1, 400000000);
     glMatrixMode(GL_MODELVIEW);
-    gluLookAt(0,0,10,0,0,0,0,1,0);
-    return;
-}
+    glLoadIdentity();
+    gluLookAt(0, -200000000, 200000000, 0, 0, 0, 0, 0, 1);
 
-void display(void){
-    glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.0,0,0);
-    glutWireTeapot(3);
+    // 绘制红色的“太阳”
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glutSolidSphere(69600000, 20, 20);
+    // 绘制蓝色的“地球”
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glRotatef(day/360.0*360.0, 0.0f, 0.0f, -1.0f);
+    glTranslatef(150000000, 0.0f, 0.0f);
+    glutSolidSphere(15945000, 20, 20);
+    // 绘制黄色的“月亮”
+    glColor3f(1.0f, 1.0f, 0.0f);
+    glRotatef(day/30.0*360.0 - day/360.0*360.0, 0.0f, 0.0f, -1.0f);
+    glTranslatef(38000000, 0.0f, 0.0f);
+    glutSolidSphere(4345000, 20, 20);
+
     glFlush();
-    return;
 }
 
-int main(int argc,char *argv[]){
-    glutInit(&argc,argv);
-    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
-    glutInitWindowPosition(0,0);
-    glutInitWindowSize(300,300);
-    glutCreateWindow("OpenGL #D View");
-    init();
-    glutDisplayFunc(display);
-    glutMainLoop();
+void myIdle(void)
+{
+    /* 新的函数，在空闲时调用，作用是把日期往后移动一天并重新绘制，达到动画效果 */
+    ++day;
+    if( day >= 360 )
+        day = 0;
+    myDisplay();
+}
 
+int main(int argc, char *argv[]) {
+    printf("== begin ==\n");
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(400, 400);
+    glutCreateWindow("T'Lab");
+    glutDisplayFunc(&myDisplay);
+    glutIdleFunc(&myIdle);
+    glutMainLoop();
+    printf("=== end ===\n");
     return 0;
 }
