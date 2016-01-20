a113456dfc6b3376b373f90d40543e27190cb472
terry <terry@rokid.com>
2016-01-20 23:04:51 +0800

    test mysqlclient lib

---
+++ HomeBase/DataBase.c
@@ -0,0 +1,6 @@
+//
+// Created by Terry on 16/1/20.
+//
+//#include <mysql.h>
+
+#include "DataBase.h"
---
+++ HomeBase/DataBase.h
@@ -0,0 +1,8 @@
+//
+// Created by Terry on 16/1/20.
+//
+
+#ifndef HOMEBASE_DATABASE_H
+#define HOMEBASE_DATABASE_H
+
+#endif //HOMEBASE_DATABASE_H
